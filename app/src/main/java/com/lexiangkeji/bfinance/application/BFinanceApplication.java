9c139075df7ee6bb1223563b8cb8197f39a29c31
fair bargain <754218265@qq.com>
2018-04-20 13:21:16 +0800

    产品详情

--- app/src/main/java/com/lexiangkeji/bfinance/application/BFinanceApplication.java
+++ app/src/main/java/com/lexiangkeji/bfinance/application/BFinanceApplication.java
@@ -2,6 +2,15 @@ package com.lexiangkeji.bfinance.application;
 
 import android.annotation.SuppressLint;
 import android.app.Application;
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import com.scwang.smartrefresh.layout.SmartRefreshLayout;
+import com.scwang.smartrefresh.layout.api.DefaultRefreshHeaderCreator;
+import com.scwang.smartrefresh.layout.api.RefreshHeader;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.scwang.smartrefresh.layout.constant.SpinnerStyle;
+import com.scwang.smartrefresh.layout.header.ClassicsHeader;
 
 /**
  * Created by 樊磊 on 2018/4/2.
@@ -16,6 +25,17 @@ public class BFinanceApplication extends Application {
     public void onCreate() {
         super.onCreate();
         mInstants = this;
+        initSwiprefresh();
+    }
+
+    private void initSwiprefresh() {
+        SmartRefreshLayout.setDefaultRefreshHeaderCreator(new DefaultRefreshHeaderCreator() {
+            @NonNull
+            @Override
+            public RefreshHeader createRefreshHeader(@NonNull Context context, @NonNull RefreshLayout layout) {
+                return new ClassicsHeader(getAppContext()).setSpinnerStyle(SpinnerStyle.Translate);
+            }
+        });
     }
 
 
--- app/src/main/java/com/lexiangkeji/bfinance/ui/activity/ProductDetailActivity.java
+++ app/src/main/java/com/lexiangkeji/bfinance/ui/activity/ProductDetailActivity.java
@@ -1,6 +1,16 @@
 package com.lexiangkeji.bfinance.ui.activity;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.lexiangkeji.bfinance.R;
 import com.lexiangkeji.bfinance.base.BaseActivity;
 
 public class ProductDetailActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_detail);
+    }
 }
